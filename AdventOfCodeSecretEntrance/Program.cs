efa42750a57e2ecbbe70943338f6e96e7e0c2c1f
Owendho <36518961+Owendho@users.noreply.github.com>
2026-02-23 11:05:45 +0100

    Add project files.

---
+++ AdventOfCodeSecretEntrance/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Might be better of making my own circular linked list from scratch
+//need to make my own circular doubly linked list 
+LinkedList<int> safeDial = new LinkedList<int>();
+safeDial.AddFirst(0);
+
+LinkedListNode<int> current = safeDial.First;
+for (int i = 1; i < 99; i++)
+{
+    safeDial.AddAfter(current, i);
+    current = safeDial.Last;
+}
+
+Console.WriteLine(safeDial.Count());
+
+//LinkedListNode<int> 
+
+//Create a method called head.previous
+
+static LinkedListNode<int> headPrevious(LinkedList<int> safeDial)
+{
+    return safeDial.Last;
+}
+
+static LinkedListNode<int> tailNext(LinkedList<int> safeDial)
+{
+    return safeDial.First;
+}
+
+//Write method to populate list
