887f20259b97fe1755e37ee80bf12f776ee35253
Owendho <36518961+Owendho@users.noreply.github.com>
2026-02-24 20:30:17 +0100

    Added selfmade doubly linked list. Need to work on traversal

---
+++ AdventOfCodeSecretEntrance/NodeRecord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCodeSecretEntrance
+{
+    public class NodeRecord
+    {
+        public int data;
+        public NodeRecord nextItem;
+        public NodeRecord previousItem;
+        public NodeRecord tail;
+        public NodeRecord head;
+    }
+}
